65b05c2aaf9a2cd39bd5d25e8dd014b3fe73a654
Thomas Sifuentes <smthomas1203@gmail.com>
2021-08-25 21:40:56 -0500

    feat: binary search as singleton method

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -5,22 +5,11 @@ import java.util.Arrays;
 public class BinarySearch {
     public static void main(String[] args) {
         int list[] = SingletonUtil.orderedList;
-        int key = SingletonUtil.findKey;
-        int result = -1;
-        int low = 0;
-        int high = list.length - 1;
-        // 1 .... 10 = 5 -> 1 + (10 - 1) / 2
-        while( low <= high){
-            int mid = low + ((high - low) / 2);
-            if(list[mid] < key) {
-                low = mid + 1;
-            } else if (list[mid] > key) {
-                high = mid - 1;
-            } else if(list[mid] == key) {
-                result = mid;
-                break;
-            }
-        }
-        System.out.println("result = " + result);
+        int findKeyMin = SingletonUtil.binarySearch(list, SingletonUtil.findKeyMin);
+        System.out.println("findKeyMin index = " + findKeyMin);
+        int findKeyMax = SingletonUtil.binarySearch(list, SingletonUtil.findKeyMax);
+        System.out.println("findKeyMax index = " + findKeyMax);
+        int notFoundKey = SingletonUtil.binarySearch(list, SingletonUtil.notFoundKey);
+        System.out.println("notFoundKey index = " + notFoundKey);
     }
 }
--- src/SingletonUtil.java
+++ src/SingletonUtil.java
@@ -5,5 +5,28 @@ public class SingletonUtil {
         return total = amount + factor;
     }
     public static int orderedList[] = {1,3,5,7,9,11,13,15,16,18};
-    public static int findKey = 10;
+    // 0,1,2,3,4,5,6,7,8,9
+    public static int findKeyMin = 1;
+    public static int findKeyMax = 18;
+    public static int notFoundKey = 10;
+
+    public static int binarySearch(int orderedList[], int key){
+        int keyIndex = -1;
+        int start = 0;
+        int end = orderedList.length - 1;
+
+        while (start <= end){
+            int mid = (end - start)/2 + start;
+            if (orderedList[mid] < key) {
+                start = mid + 1;
+            } else if (orderedList[mid] > key) {
+                end = mid - 1;
+            } else if(orderedList[mid] == key) {
+                keyIndex = mid;
+                break;
+            }
+        }
+
+        return keyIndex;
+    }
 }
