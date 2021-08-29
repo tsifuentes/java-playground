4b85a51f77bbe87ff1667d8fc1c129c279ac38fe
Thomas Sifuentes <smthomas1203@gmail.com>
2021-08-28 23:17:19 -0500

    feat: playing with String method first part

--- src/StringPractice.java
+++ src/StringPractice.java
@@ -1,5 +1,27 @@
 public class StringPractice {
     public static void main(String[] args) {
+
+        comparison();
+        concatenate();
+        immutable();
+
+        getTimingConcat(500);
+        getTimingConcat(1000);
+        getTimingConcat(10000);
+        getTimingConcat(100000);
+        getTimingLiteral(500);
+        getTimingLiteral(1000);
+        getTimingLiteral(10000);
+        getTimingLiteral(100000);
+        getTimingBuilder(500);
+        getTimingBuilder(1000);
+        getTimingBuilder(10000);
+        getTimingBuilder(100000);
+
+        allMethods();
+    }
+
+    public static void comparison(){
         String stringA = "First literal string";
         String stringB = new String("First literal string");
         String stringC = "FIRST LITERAL STRING";
@@ -16,9 +38,6 @@ public class StringPractice {
 
         isEqual = stringA == stringAA;
         System.out.println("isEqual Same literal value= " + isEqual);
-
-        concatenate();
-        immutable();
     }
 
     public static void concatenate(){
@@ -53,19 +72,6 @@ public class StringPractice {
         String replaced = student.replace("e", "E").replace("s", "S");
         System.out.println("student = " + student);
         System.out.println("replaced = " + replaced);
-
-        getTimingConcat(500);
-        getTimingConcat(1000);
-        getTimingConcat(10000);
-        getTimingConcat(100000);
-        getTimingLiteral(500);
-        getTimingLiteral(1000);
-        getTimingLiteral(10000);
-        getTimingLiteral(100000);
-        getTimingBuilder(500);
-        getTimingBuilder(1000);
-        getTimingBuilder(10000);
-        getTimingBuilder(100000);
     }
 
     public static void getTimingConcat(long times){
@@ -131,5 +137,19 @@ public class StringPractice {
         if(isBlank) {
             System.out.println("stringB = " + stringB);
         }
+    }
+
+    public static void allMethods() {
+        String fullName = "Thomas Sifuentes";
+        System.out.println("fullName.toUpperCase() = " + fullName.toUpperCase());
+        System.out.println("fullName.toLowerCase() = " + fullName.toLowerCase());
+        System.out.println("fullName.equals(\"Thomas Sifuentes\") = " + fullName.equals("Thomas Sifuentes"));
+        System.out.println("fullName.equalsIgnoreCase(\"thomas sifuentes\") = " + fullName.equalsIgnoreCase("thomas sifuentes"));
+        System.out.println("fullName.compareTo(\"Andres\") = " + fullName.compareTo("Andres"));
+        System.out.println("fullName.compareTo(\"Thomas Sifuentes\") = " + fullName.compareTo("Thomas Sifuentes"));
+        System.out.println("fullName.compareTo(\"Bruno\") = " + fullName.compareTo("Yessenia"));
+        System.out.println("fullName.substring(2) = " + fullName.substring(2));
+        System.out.println("fullName.substring(2) = " + fullName.substring(3,8));
+        System.out.println("fullName.charAt(10) = " + fullName.charAt(10));
     }
 }
