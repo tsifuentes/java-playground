06b82999d376dedd462d6892b35854ea54e903a3
Thomas Sifuentes <smthomas1203@gmail.com>
2021-09-01 23:43:23 -0500

    feat: playing with if else for a leap year control flow

--- src/ControlFlow.java
+++ src/ControlFlow.java
@@ -2,7 +2,17 @@ import javax.swing.*;
 
 public class ControlFlow {
     public static void main(String[] args) {
-        checkYourGrade();
+        int option = Integer.parseInt(JOptionPane.showInputDialog("Choose an option: \n" +
+                "1. Check your grade\n" +
+                "2. Check a leap year\n"));
+        switch (option) {
+            case 1:
+                checkYourGrade();
+                break;
+            case 2:
+                checkLeapYear();
+                break;
+        }
     }
 
     public static void checkYourGrade(){
@@ -18,6 +28,15 @@ public class ControlFlow {
             System.out.println("Approved!, but you need to study harder");
         } else if (grade < 10) {
             System.out.println("Sorry, you have disqualified.");
+        }
+    }
+
+    public static void checkLeapYear(){
+        int year = Integer.parseInt(JOptionPane.showInputDialog("Enter a year"));
+        if(year % 400 == 0 || (year % 4 == 0) && !(year % 100 == 0)){
+            System.out.println("It is a Leap Year!");
+        } else {
+            System.out.println("Oww! It is not a leap year!");
         }
     }
 }
