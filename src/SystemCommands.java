75606c1d044f0a804129973fd0410bfe36b886d5
Thomas Sifuentes <smthomas1203@gmail.com>
2021-09-07 23:54:25 -0500

    feat: playing with System get properties

--- src/SystemCommands.java
+++ src/SystemCommands.java
@@ -1,8 +1,11 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.Properties;
 
 public class SystemCommands {
     public static void main(String[] args) {
         props();
+        getProperties();
     }
 
     public static void props() {
@@ -23,5 +26,22 @@ public class SystemCommands {
 
         String lineSeparator2 = System.lineSeparator();
         System.out.println("lineSeparator2 = " + lineSeparator2);
+    }
+
+    public static void getProperties(){
+        try {
+            FileInputStream config = new FileInputStream("src/config.properties");
+            Properties properties = new Properties(System.getProperties());
+            properties.load(config);
+            properties.setProperty("config.set.value", "THSMIT");
+
+            System.setProperties(properties);
+            Properties p = System.getProperties();
+            p.list(System.out);
+            System.out.println("System.getProperty(\"config.set.value\") = " + System.getProperty("config.set.value"));
+        } catch (Exception e) {
+            System.out.println("File does not exist = " + e);
+        }
+
     }
 }
