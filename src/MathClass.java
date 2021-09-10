808e6fc33e34057dda1e8b8150281d08deaf56d6
Thomas Sifuentes <smthomas1203@gmail.com>
2021-09-09 23:48:43 -0500

    feat: playing with Math basic methods

---
+++ src/MathClass.java
@@ -0,0 +1,20 @@
+public class MathClass {
+    public static void main(String[] args) {
+        int absolut = Math.abs(-12);
+        System.out.println("absolut = " + absolut);
+
+        double max = Math.max(1.3, 2.3);
+        System.out.println("max = " + max);
+
+        double min = Math.min(4.5, 1.1);
+        System.out.println("min = " + min);
+
+        double ceil = Math.ceil(2.4);
+        System.out.println("ceil = " + ceil);
+
+        double floor = Math.floor(3.5);
+        System.out.println("floor 3.5 = " + floor);
+
+        System.out.println("Math.PI = " + Math.PI);
+    }
+}
