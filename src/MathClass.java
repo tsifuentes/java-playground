3e0510a2725476ea9892cd0750135cd87417da55
Thomas Sifuentes <smthomas1203@gmail.com>
2021-09-09 23:51:55 -0500

    feat: playing with Math trigo

--- src/MathClass.java
+++ src/MathClass.java
@@ -16,5 +16,19 @@ public class MathClass {
         System.out.println("floor 3.5 = " + floor);
 
         System.out.println("Math.PI = " + Math.PI);
+
+        double sq = Math.sqrt(4);
+        System.out.println("sq = " + sq);
+
+        double degree = Math.toDegrees(1.57);
+        System.out.println("degree = " + degree);
+        degree = Math.round(degree);
+        System.out.println("round degree = " + degree);
+
+        double radians = Math.toRadians(90.00);
+        System.out.println("radians = " + radians);
+
+        System.out.println("Math.sin(radians) = " + Math.sin(radians));
+        System.out.println("Math.cos(radians) = " + Math.cos(radians));
     }
 }
