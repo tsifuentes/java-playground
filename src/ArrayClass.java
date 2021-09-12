86e9aeb489ca28d467c071dc2142a32ba2ccc0a5
Thomas Sifuentes <smthomas1203@gmail.com>
2021-09-11 23:11:34 -0500

    feat: playing with Arrays with strings

--- src/ArrayClass.java
+++ src/ArrayClass.java
@@ -1,6 +1,10 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class ArrayClass {
     public static void main(String[] args) {
         arrayIntegers();
+        arrayStrings();
     }
 
     public static void arrayIntegers(){
@@ -13,6 +17,21 @@ public class ArrayClass {
 
         for (int number: numbers){
             System.out.println("number = " + number);
+        }
+    }
+
+    public static void arrayStrings(){
+        String[] strings = new String[5];
+
+        strings[0] = "Kingston pendrive 440";
+        strings[1] = "Samsung Galaxy";
+        strings[2] = "Hard Disk SSD SE";
+        strings[3] = "ASUS Notebook";
+        strings[4] = "Macbook Air";
+
+        Arrays.sort(strings);
+        for(String string: strings){
+            System.out.println("string = " + string);
         }
     }
 }
